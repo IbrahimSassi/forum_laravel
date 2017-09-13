71e66245f4ce04c3844226ce93b02ceea314afd6
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-09-13 19:53:22 +0100

    Initial DB + seeding

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,23 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(\App\Thread::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => function () {
+            return factory(\App\User::class)->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(\App\Reply::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => function () {
+            return factory(\App\User::class)->create()->id;
+        },
+        'body' => $faker->sentence
+    ];
+});
