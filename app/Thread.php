afa633e3de53a98cec58befe84fe8f75f4afcb57
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-09-30 23:30:59 +0100

    a big of refactoring

--- app/Thread.php
+++ app/Thread.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Events\ThreadHasNewReply;
 use App\Filters\ThreadFilters;
 use Illuminate\Database\Eloquent\Model;
 
@@ -72,12 +73,8 @@ class Thread extends Model
     {
         $reply = $this->replies()->create($reply);
 
-        // Prepare notifications for all subscribers.
-        $this->subscriptions
-            ->filter(function ($sub) use ($reply) {
-                return $sub->user_id != $reply->user_id;
-            })
-            ->each->notify($reply);
+        event(new ThreadHasNewReply($this,$reply));
+
 
         return $reply;
 
